7f301ed4079c341041a614caec2c314e880cecc0
MrFabulous <fabian@die-baders.de>
2022-06-12 19:28:25 +0200

    wm1 adjusted, added welcome functionallity

--- src/extensions/welcome/welcome.py
+++ src/extensions/welcome/welcome.py
@@ -11,23 +11,23 @@ import lightbulb
 
 
 with open(os.path.normpath(f"{os.getcwd()}/src/extensions/welcome/config.json"), "r") as f:
-    config = json.load(f)
+    config = json.load(f)   #auf config kann per config["3"]
 
-welcome_plugin = lightbulb.Plugin("Welcome")
+welcome_plugin = lightbulb.Plugin("Welcome") #Plugin generieren um welcome einzeln deaktivieren zu können ohne Bot funktionalitäten zu beinflussen.
 
-@welcome_plugin.command
-@lightbulb.command("welcome", "bar")
+@welcome_plugin.command #Z18-22 Erstellt Gruppe /welcome mit Beschreibung
+@lightbulb.command("welcome", "Module for welcome messages") #Beschreibt Modul, erster Wert Blockbezeichnung im Slash Command, zweiter Beschreibung.
 @lightbulb.implements(lightbulb.SlashCommandGroup)
 async def welcome_group(ctx: lightbulb.Context) -> None:
     pass  # as slash commands cannot have their top-level command ran, we simply pass here
 
-@welcome_group.child
-@lightbulb.option("optionname", "description")
-@lightbulb.command("commandname", "description", auto_defer=True)
+@welcome_group.child #Unterordnung unter welcome Group aus 18-22
+#@lightbulb.option("optionname", "description")
+@lightbulb.command("send", "description", auto_defer=True)
 @lightbulb.implements(lightbulb.SlashSubCommand)
-async def commandname(ctx: lightbulb.Context) -> None:
+async def send(ctx: lightbulb.Context) -> None: #Command wird als Methode erstellt
     await ctx.respond(hikari.ResponseType.DEFERRED_MESSAGE_CREATE) # temporary fix as auto_defer doesn't work rn
-    await ctx.respond('This is a response!')
+    await ctx.respond(config["interaction"]["welcome message 1"]) #Bot Antwort aus config
 
 def load(bot: lightbulb.BotApp) -> None:
     bot.add_plugin(welcome_plugin)
